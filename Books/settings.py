b898f26d47aab70cee4ae838dbcb4e03e2ca724b
meeklife <forsonbaah@gmail.com>
2023-11-27 15:26:11 +0000

    Ready for deployment

--- Books/settings.py
+++ Books/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u7bh!sqm)yrh60-+0=8+h&&c#u8w5uxntwnzs!tc)uv($oivcc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
