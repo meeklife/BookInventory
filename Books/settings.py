347f0a6e02424a3390bbd48c25c2cd6ef8ee6f3d
meeklife <forsonbaah@gmail.com>
2023-12-06 12:34:54 +0000

    different settings for redeployment

--- Books/settings.py
+++ Books/settings.py
@@ -9,48 +9,57 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
+import dotenv
+import dj_database_url
+# import environ
+
+from dotenv import load_dotenv
 from pathlib import Path
 
-import environ
-import os
-env = environ.Env()
+load_dotenv()
+# env = environ.Env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == True
 
-ALLOWED_HOSTS = []
+# "https://bookinventory-crimson-feather-8949.fly.dev", "localhost", "127.0.0.1"]
+# APP_NAME = os.environ.get("FLY_APP_NAME")
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'bookinventory.fly.dev']
+CSRF_TRUSTED_ORIGINS = ['https://bookinventory.fly.dev']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'bookApi',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
-    'bookApi',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'drf_yasg',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,14 +93,11 @@ WSGI_APPLICATION = 'Books.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join('db.sqlite3')
+    )
+
+
 }
 
 
@@ -128,6 +134,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoisestorage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = 'static/'
 
